93a38c358eb40cbd5bbee3bdb42290766ba4819c
sopeauh <sophia.thach@gmail.com>
2015-04-28 01:47:14 -0400

    instantialized posts

--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,7 @@ from .models import Post
 # Create your views here.
 def post_list(request):
 	#order the list by date
-	Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+
 	return render(request, 'blog/post_list.html',{'posts':posts})
 
