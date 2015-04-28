af9164fe3017468412da99891cbd6bafa280bdd1
sopeauh <sophia.thach@gmail.com>
2015-04-28 01:19:09 -0400

    for loop to display the posts

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+#import the time
+from django.utils import timezone
+#import the Post class 
+from .models import Post
 
 # Create your views here.
 def post_list(request):
-	return render(request, 'blog/post_list.html',{})
+	#order the list by date
+	Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'blog/post_list.html',{'posts':posts})
 
