5652e864397f183efd47698b43183096d61c1fdf
sopeauh <sophia.thach@gmail.com>
2015-04-27 22:53:05 -0400

    fixed error in settings.. typo

--- mysite/settings.py
+++ mysite/settings.py
@@ -108,7 +108,7 @@ STATIC_URL = '/static/'
 
 
 import dj_database_url
-DATABASES['default']=dj_database+url.config()
+DATABASES['default']=dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER=('HTTP_X_FORWARDED_PROTO', 'https')
 
