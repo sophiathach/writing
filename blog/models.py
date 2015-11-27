42019a19c70ec23245d702531f80a5918e479c94
Sophia Thach <sophia.thach@gmail.com>
2015-11-27 16:34:36 -0500

    literature fields

--- blog/models.py
+++ blog/models.py
@@ -4,10 +4,17 @@
 from django.db import models
 from django.utils import timezone
 
+LIT_TYPE = (  
+    ('PM', 'poem'),
+    ('SS', 'short story'),
+    ('ES', 'essay'),
+    ('DR', 'drama'),
+)
 #models.Model meant thathe Post is a Django model so it knows to save it
 class Post(models.Model):
 	author=models.ForeignKey('auth.User')
 	title=models.CharField(max_length=200)
+	literature_type=models.CharField(max_length=2, choices=LIT_TYPE)
 	text=models.TextField()
 	created_date=models.DateTimeField(
 		 default=timezone.now)
